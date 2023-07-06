dea22d7748a472d759cd6d699ba0709f01b1caed
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2023-07-06 11:13:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ const lbs = document.querySelector(".lbs");
 const BMIScore = document.getElementById("BMI-score");
 const metricButton = document.querySelector(".calc-bmi-metric");
 const imperialButton = document.querySelector(".calc-bmi-imperial");
+const BMIRange = document.querySelector(".BMI-range");
+const BMIPlaceholder = document.querySelector(".BMI-placeholder");
+
+BMIPlaceholder.textContent = `Welcome
+Enter your height and weight and you’ll see your BMI result here
+    `;
 
 const measureChecked = function () {
   const isMetricSelected = metric.checked;
@@ -26,53 +32,103 @@ const measureChecked = function () {
   imperialHeight2.classList.toggle("active", !isMetricSelected);
   imperialWeight1.classList.toggle("active", !isMetricSelected);
   imperialWeight2.classList.toggle("active", !isMetricSelected);
-  if (isMetricSelected) {
-    details.style.gridTemplateColumns = "1fr";
-    details.style.gridTemplateRows = "1fr 1fr";
-    cm.style.display = "block";
-    kg.style.display = "block";
-    ft.style.display = "none";
-    inch.style.display = "none";
-    st.style.display = "none";
-    lbs.style.display = "none";
-    metricButton.style.display = "block";
-    imperialButton.style.display = "none";
-  } else {
-    details.style.gridTemplateColumns = "1fr 1fr";
-    details.style.gridTemplateRows = "1fr";
-    cm.style.display = "none";
-    kg.style.display = "none";
-    ft.style.display = "block";
-    inch.style.display = "block";
-    st.style.display = "block";
-    lbs.style.display = "block";
-    metricButton.style.display = "none";
-    imperialButton.style.display = "block";
-  }
+  details.style.gridTemplateColumns = isMetricSelected ? "1fr" : "1fr 1fr";
+  details.style.gridTemplateRows = isMetricSelected ? "1fr 1fr" : "1fr";
+  cm.style.display = isMetricSelected ? "block" : "none";
+  kg.style.display = isMetricSelected ? "block" : "none";
+  ft.style.display = isMetricSelected ? "none" : "block";
+  inch.style.display = isMetricSelected ? "none" : "block";
+  st.style.display = isMetricSelected ? "none" : "block";
+  lbs.style.display = isMetricSelected ? "none" : "block";
 };
 imperial.addEventListener("change", measureChecked);
 metric.addEventListener("change", measureChecked);
 measureChecked();
 
-const getMetricMeasurements = function () {
-  const b = Number(metricHeight.value);
-  const a = Number(metricWeight.value);
-  const CalcBMI = function (weight, height) {
-    return (BMIValue = weight / ((height / 100) * (height / 100)));
-  };
-
-  BMIScore.textContent = `${CalcBMI(a, b)}`;
+const CalcBMI = function (weight, height, isMetric) {
+  if (isMetric) {
+    return weight / ((height / 100) * (height / 100));
+  } else {
+    return (weight / (height * height)) * 703;
+  }
 };
 
-const getImperialMeasurements = function () {
+const getMeasurements = function (isMetric) {
   const ft = Number(imperialHeight1.value);
   const inch = Number(imperialHeight2.value);
   const st = Number(imperialWeight1.value);
   const lbs = Number(imperialWeight2.value);
-  const inchSum = ft * 12 + inch;
-  const lbsSum = st * 14 + lbs;
-  const CalcBMI = function (weight, height) {
-    return (BMIValue = (weight / (height * height)) * 703);
-  };
-  BMIScore.textContent = `${CalcBMI(lbsSum, inchSum)}`;
+  const imperialWeight = st * 14 + lbs;
+  const imperialHeight = ft * 12 + inch;
+
+  const inputWeight = isMetric ? Number(metricWeight.value) : imperialWeight;
+  const inputHeight = isMetric ? Number(metricHeight.value) : imperialHeight;
+
+  const BMI = CalcBMI(inputHeight, inputWeight, isMetric);
+  console.log(BMI, inputHeight, inputWeight);
+
+  BMIScore.textContent = BMI.toFixed(2);
+
+  if (CalcBMI(inputWeight, inputHeight) < 18.5) {
+    BMIPlaceholder.textContent = `Your BMI is...`;
+    BMIRange.textContent = ` Your BMI suggests you're
+  Underweight. Your ideal weight is between
+  <!-- add range -->.`;
+  } else if (
+    CalcBMI(inputWeight, inputHeight) >= 18.5 &&
+    CalcBMI(inputWeight, inputHeight) <= 24.9
+  ) {
+    BMIPlaceholder.textContent = `Your BMI is...`;
+    BMIRange.textContent = ` Your BMI suggests you're
+  Healthy weight. Your ideal weight is between
+  <!-- add range -->.`;
+  } else if (
+    CalcBMI(inputWeight, inputHeight) >= 25 &&
+    CalcBMI(inputWeight, inputHeight) <= 29.9
+  ) {
+    BMIPlaceholder.textContent = `Your BMI is...`;
+    BMIRange.textContent = ` Your BMI suggests you're
+  Overweight. Your ideal weight is between
+  <!-- add range -->.`;
+  } else if (CalcBMI(inputWeight, inputHeight) > 30) {
+    BMIPlaceholder.textContent = `Your BMI is...`;
+    BMIRange.textContent = ` Your BMI suggests you're
+  Obese. Your ideal weight is between
+  <!-- add range -->.`;
+  } else {
+    BMIScore.textContent = "";
+    BMIPlaceholder.textContent = `Welcome
+  Enter your height and weight and you’ll see your BMI result here
+  `;
+  }
+
+  if (BMI === Infinity) {
+    BMIScore.textContent = "";
+    BMIPlaceholder.textContent = `Welcome
+  Enter your height and weight and you’ll see your BMI result here
+  `;
+    BMIRange.textContent = "";
+  }
 };
+
+metricWeight.addEventListener("blur", function () {
+  getMeasurements(true);
+});
+
+metricHeight.addEventListener("blur", function () {
+  getMeasurements(true);
+});
+imperialHeight1.addEventListener("blur", function () {
+  getMeasurements(false);
+});
+
+imperialHeight2.addEventListener("blur", function () {
+  getMeasurements(false);
+});
+imperialWeight1.addEventListener("blur", function () {
+  getMeasurements(false);
+});
+
+imperialWeight2.addEventListener("blur", function () {
+  getMeasurements(false);
+});
